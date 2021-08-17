fe46ba08aa7b8a934fb6c58b82b04ed8a0554e0a
Rahim <88458381+bahrahim@users.noreply.github.com>
2021-08-17 11:25:04 +0200

    Patineur

--- index.js
+++ index.js
@@ -53,3 +53,19 @@ if (theme.getAttribute("href") != "css/square.css") {
 		theme.setAttribute("href", "css/square.css");
 }
 
+// le patineur 
+
+function patineur() {
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	var theme = document.getElementById("change-style");
+
+	// Change the value of href attribute
+	// to change the css sheet.
+	if (theme.getAttribute("href") != "css/patineur.css") {
+		theme.setAttribute("href", "css/patineur.css");
+	} else {
+		theme.setAttribute("href", "css/patineur.css");
+	}
+}
