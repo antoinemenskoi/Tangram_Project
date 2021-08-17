726bc749d0aa730e9fb61cb30a29f1e7d23bddf1
Rahim <88458381+bahrahim@users.noreply.github.com>
2021-08-17 15:27:37 +0200

    getfoh

--- index.js
+++ index.js
@@ -80,3 +80,37 @@ if (theme.getAttribute("href") != "css/square.css") {
 } else {
 		theme.setAttribute("href", "css/square.css");
 }
+
+// le patineur
+
+function patineur() {
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	var theme = document.getElementById("change-style");
+
+	// Change the value of href attribute
+	// to change the css sheet.
+	if (theme.getAttribute("href") != "css/patineur.css") {
+		theme.setAttribute("href", "css/patineur.css");
+	} else {
+		theme.setAttribute("href", "css/patineur.css");
+	}
+}
+
+// la danseuse
+
+function danseuse() {
+	// Obtains an array of all <link>
+	// elements.
+	// Select your element using indexing.
+	var theme = document.getElementById("change-style");
+
+	// Change the value of href attribute
+	// to change the css sheet.
+	if (theme.getAttribute("href") != "css/danseuse.css") {
+		theme.setAttribute("href", "css/danseuse.css");
+	} else {
+		theme.setAttribute("href", "css/danseuse.css");
+	}
+}
