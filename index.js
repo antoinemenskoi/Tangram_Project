d2c9632296984f55507bc893dde3d00440d5a02a
fanny modiano <fanny13-m@hotmail.fr>
2021-08-23 11:14:55 +0200

    final project

--- index.js
+++ index.js
@@ -1,86 +1,109 @@
-// function index() {
-// 	// Obtains an array of all <link>
-// 	// elements.
-// 	// Select your element using indexing.
-// 	var theme = document.getElementById("change-style");
+/*index */
 
-// 	// Change the value of href attribute
-// 	// to change the css sheet.
-// 	if (theme.getAttribute("href") != "css/index.css") {
-// 		theme.setAttribute("href", "css/index.css");
-// 	} else {
-// 		theme.setAttribute("href", "css/index.css");
-// 	}
-// }
-// function square() {
-// 	// Obtains an array of all <link>
-// 	// elements.
-// 	// Select your element using indexing.
-// 	var theme = document.getElementById("change-style");
+const dropdownchild = document.querySelector('.dropdown-child');
+dropdownchild.addEventListener('click', () => {
+	dropdownchild.classList.toggle('active');
+});
 
-// 	// Change the value of href attribute
-// 	// to change the css sheet.
-// 	if (theme.getAttribute("href") != "css/square.css") {
-// 		theme.setAttribute("href", "css/square.css");
-// 	} else {
-// 		theme.setAttribute("href", "css/square.css");
-// 	}
-// }
+function index() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/index.css") {
+		theme.setAttribute("href", "css/index.css");
+	} else {
+		theme.setAttribute("href", "css/index.css");
+	}
+}
 
-// function poule() {
-// 	// Obtains an array of all <link>
-// 	// elements.
-// 	// Select your element using indexing.
-// 	var theme = document.getElementById("change-style");
+/*Square */
+function square() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/square.css") {
+		theme.setAttribute("href", "css/square.css");
+	} else {
+		theme.setAttribute("href", "css/square.css");
+	}
+}
 
-// 	// Change the value of href attribute
-// 	// to change the css sheet.
-// 	if (theme.getAttribute("href") != "css/poule.css") {
-// 		theme.setAttribute("href", "css/poule.css");
-// 	} else {
-// 		theme.setAttribute("href", "css/poule.css");
-// 	}
-// }
-// 	// Obtains an array of all <link>
-// 	// elements.
-// 	// Select your element using indexing.
-// var theme = document.getElementById("change-style");
+/*poule */
+function poule() {
 
-// // Change the value of href attribute
-// // to change the css sheet.
-// if (theme.getAttribute("href") != "css/square.css") {
-// 	theme.setAttribute("href", "css/square.css");
-// } else {
-// 		theme.setAttribute("href", "css/square.css");
-// }
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/poule.css") {
+		theme.setAttribute("href", "css/poule.css");
+	} else {
+		theme.setAttribute("href", "css/poule.css");
+	}
+}
 
-// function chat() {
-// 	// Obtains an array of all <link>
-// 	// elements.
-// 	// Select your element using indexing.
-// 	var theme = document.getElementById("change-style");
 
-// 	// Change the value of href attribute
-// 	// to change the css sheet.
-// 	if (theme.getAttribute("href") != "css/chat.css") {
-// 		theme.setAttribute("href", "css/chat.css");
-// 	} else {
-// 		theme.setAttribute("href", "css/chat.css");
-// 	}
-// }
-// 	// Obtains an array of all <link>
-// 	// elements.
-// 	// Select your element using indexing.
-// var theme = document.getElementById("change-style");
+/*chat */
+function chat() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/chat.css") {
+		theme.setAttribute("href", "css/chat.css");
+	} else {
+		theme.setAttribute("href", "css/chat.css");
+	}
+}
 
-// // Change the value of href attribute
-// // to change the css sheet.
-// if (theme.getAttribute("href") != "css/square.css") {
-// 	theme.setAttribute("href", "css/square.css");
-// } else {
-// 		theme.setAttribute("href", "css/square.css");
-// }
-function swap(cssFile) {
-    var link = document.getElementById("change-style")
-    link.href = cssFile;
+
+/*pyramides */
+function pyramides() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/pyramides.css") {
+		theme.setAttribute("href", "css/pyramides.css");
+	} else {
+		theme.setAttribute("href", "css/pyramides.css");
+	}
+}
+
+/*oiseau */
+function oiseau() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/oiseau.css") {
+		theme.setAttribute("href", "css/oiseau.css");
+	} else {
+		theme.setAttribute("href", "css/oiseau.css");
+	}
+}
+
+/*avion */
+function avion() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/avion.css") {
+		theme.setAttribute("href", "css/avion.css");
+	} else {
+		theme.setAttribute("href", "css/avion.css");
+	}
+}
+
+/*girafe */
+function girafe() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/girafe.css") {
+		theme.setAttribute("href", "css/girafe.css");
+	} else {
+		theme.setAttribute("href", "css/girafe.css");
+	}
+}
+
+
+/*kangourou */
+function kangourou() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/kangourou.css") {
+		theme.setAttribute("href", "css/kangourou.css");
+	} else {
+		theme.setAttribute("href", "css/kangourou.css");
+	}
+}
+
+/*danceuse */
+function danseuse() {
+	var theme = document.getElementById("change-style");
+	if (theme.getAttribute("href") != "css/danseuse.css") {
+		theme.setAttribute("href", "css/danseuse.css");
+	} else {
+		theme.setAttribute("href", "css/danseuse.css");
+	}
 }
